8f9d3e61d1e5e260c4d5d8d981e181232a2a8087
samkayes <samanthasubaan@gmail.com>
2023-08-27 20:52:58 +0800

    Added files

---
+++ script.js
@@ -0,0 +1,12 @@
+function addListItem() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("list").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+}
